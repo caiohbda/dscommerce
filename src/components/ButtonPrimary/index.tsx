bd46b60aa004115eab367623deec40ebb752c6b9
Caio Henrique <caiohbda777@gmail.com>
2024-05-08 00:41:10 -0300

    React Components Inicial

---
+++ src/components/ButtonPrimary/index.tsx
@@ -0,0 +1,5 @@
+import './style.css';
+
+export default function ButtonPrimary() {
+  return (<div className="dsc-btn dsc-btn-blue">Comprar</div>);
+}
---
+++ src/components/ProductCategory/index.tsx
@@ -0,0 +1,9 @@
+import './style.css'
+
+export default function ProductCategory(){
+  return(
+    <div className="dsc-category-container">
+          <div className="dsc-category">Eletrônicos</div>
+    </div>
+  );
+}
